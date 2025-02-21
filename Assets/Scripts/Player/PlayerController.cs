99a463cb972a13d6e9974b58911806a67c0861fe
Gabe Karras <gkarras@emich.edu>
2025-02-21 14:16:57 -0500

    Added player tilt animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,22 +8,37 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float speed = 4;
 
+    // X coordinates for animation
+    private float x;
+    private float previousX;
+
+    private Animator playerAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
         // Divide for vector math
         speed = speed / 1000;
-    
+
+        x = transform.position.x;
+        previousX = x;
+
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movePlayer();
+        MovePlayer();
+
+        // Update x coordinates for animate function
+        x = transform.position.x;
+        AnimatePlayer();
+        previousX = x;
     }
 
     // Move player with arrow keys
-    public void movePlayer() {
+    public void MovePlayer() {
         if (Input.GetKey(KeyCode.RightArrow))
             transform.position += new Vector3(speed, 0, 0);
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -32,5 +47,16 @@ public class PlayerController : MonoBehaviour
             transform.position += new Vector3(0, speed, 0);
         if (Input.GetKey(KeyCode.DownArrow))
             transform.position += new Vector3(0, -1 * speed, 0);
+    }
+
+    // Tilt the ship if it's going in a certain direction
+    public void AnimatePlayer() {
+        if (x < previousX) {
+            playerAnimator.Play("PlayerLeft");
+        } else if (x > previousX) {
+            playerAnimator.Play("PlayerRight");
+        } else {
+            playerAnimator.Play("PlayerIdle");
+        }
     }
 }
